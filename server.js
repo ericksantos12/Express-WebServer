c64113000d429530981ecf2703002d008d803bd8
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-04-03 16:02:00 -0300

    🎨 added port redundancy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config({
     path: ".env"
 })
 
-app.set('port', process.env.port);
+app.set('port', process.env.port || 8080);
 const server = app.listen(app.get('port'), ()=>{
     console.log(`Servidor Rodando em: http://localhost:${server.address().port}`);
 })
