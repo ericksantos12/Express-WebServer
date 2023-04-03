5f5f121f98bcdcc11a1486f027bb3bb5d397e493
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-04-03 15:40:04 -0300

    ✨ added new routes

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const router = express.Router()
 router.get('/', (req, res) =>{
     res.send("Ola Mundo")
 })
+router.get('/rota1', (req, res) =>{
+    res.send("rota1 :D")
+})
+router.get('/rota2', (req, res) =>{
+    res.send("rota2 :O")
+})
+router.get('/rota1/vinicius', (req, res) =>{
+    res.send("Esta que é a grande sacada!")
+})
+router.get('/rota2/minecraft', (req, res) =>{
+    res.send("1.20 performance update!!")
+})
 
 
 
